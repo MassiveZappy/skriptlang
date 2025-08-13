9257bf6794967074e44f38dd596f72f9ee4325b5
MassiveZappy <massivezappy@gmail.com>
2025-08-13 18:21:33 -0400

    hey

--- target/debug/deps/streaming_iterator-14fd22e07d6096a1.d
+++
@@ -1,7 +0,0 @@
-/Users/massivezappy/Documents/zedSkript/grammars/skript/target/debug/deps/streaming_iterator-14fd22e07d6096a1.d: /Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/lib.rs /Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/slice.rs /Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/sources.rs
-
-/Users/massivezappy/Documents/zedSkript/grammars/skript/target/debug/deps/libstreaming_iterator-14fd22e07d6096a1.rmeta: /Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/lib.rs /Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/slice.rs /Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/sources.rs
-
-/Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/lib.rs:
-/Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/slice.rs:
-/Users/massivezappy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/streaming-iterator-0.1.9/src/sources.rs:
