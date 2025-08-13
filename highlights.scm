3b9f7f643ee78c6cf48a1c3bfb6efce4c772f094
MassiveZappy <massivezappy@gmail.com>
2025-08-13 10:17:38 -0400

    Checking something with basic json formatting

--- highlights.scm
+++ highlights.scm
@@ -1,99 +1,106 @@
-; Comments
-(comment) @comment
-
-; Keywords
-[
-  "on"
-  "options"
-  "command"
-  "trigger"
-  "permission"
-  "set"
-  "if"
-  "else"
-  "loop"
-  "while"
-  "try"
-  "catch"
-  "wait"
-  "give"
-  "send"
-  "broadcast"
-  "increment"
-  "decrement"
-  "stop"
-  "exit"
-  "every"
-  "to"
-  "of"
-  "is"
-  "has"
-  "does"
-  "not"
-  "by"
-  "between"
-  "in"
-  "around"
-  "and"
-  "or"
-] @keyword
-
-; Operators
-[
-  "+"
-  "-"
-  "="
-  "=="
-  "!="
-  ">"
-  "<"
-  ">="
-  "<="
-] @operator
-
-; Special keywords for events
-(event_type) @keyword.special
-
-; Special identifiers
-(permission_identifier) @variable.special
-(options_block) @variable.special
-
-; Variables
-(variable) @variable
-
-; Placeholders (expressions in %)
-(placeholder) @string.special
-
-; String literals
 (string) @string
 
-; Numbers
+(pair
+  key: (string) @property.json_key)
+
 (number) @number
 
-; Boolean literals
-[
-  (true)
-  (false)
-] @boolean
+; ; Comments
+; (comment) @comment
 
-; Time spans
-(time_span) @number.special
+; ; Keywords
+; [
+;   "on"
+;   "options"
+;   "command"
+;   "trigger"
+;   "permission"
+;   "set"
+;   "if"
+;   "else"
+;   "loop"
+;   "while"
+;   "try"
+;   "catch"
+;   "wait"
+;   "give"
+;   "send"
+;   "broadcast"
+;   "increment"
+;   "decrement"
+;   "stop"
+;   "exit"
+;   "every"
+;   "to"
+;   "of"
+;   "is"
+;   "has"
+;   "does"
+;   "not"
+;   "by"
+;   "between"
+;   "in"
+;   "around"
+;   "and"
+;   "or"
+; ] @keyword
 
-; Functions/methods
-(function_call) @function
+; ; Operators
+; [
+;   "+"
+;   "-"
+;   "="
+;   "=="
+;   "!="
+;   ">"
+;   "<"
+;   ">="
+;   "<="
+; ] @operator
 
-; Types
-(type) @type
+; ; Special keywords for events
+; (event_type) @keyword.special
 
-; Sections
-[
-  (trigger_block)
-  (options_block)
-] @keyword.special
+; ; Special identifiers
+; (permission_identifier) @variable.special
+; (options_block) @variable.special
 
-; Commands
-(command_name) @function.special
+; ; Variables
+; (variable) @variable
 
-; Event declarations
-(event_declaration
-  name: (_) @function.macro)
+; ; Placeholders (expressions in %)
+; (placeholder) @string.special
+
+; ; String literals
+; (string) @string
+
+; ; Numbers
+; (number) @number
+
+; ; Boolean literals
+; [
+;   (true)
+;   (false)
+; ] @boolean
+
+; ; Time spans
+; (time_span) @number.special
+
+; ; Functions/methods
+; (function_call) @function
+
+; ; Types
+; (type) @type
+
+; ; Sections
+; [
+;   (trigger_block)
+;   (options_block)
+; ] @keyword.special
+
+; ; Commands
+; (command_name) @function.special
+
+; ; Event declarations
+; (event_declaration
+;   name: (_) @function.macro)
